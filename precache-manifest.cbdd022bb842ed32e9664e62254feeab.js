34015f6331d63b903a429fddbf8ae0a38b8ebde0
Katia Velasquez <katia.velasquez@.com>
2023-01-16 17:51:49 -0300

    Updates

---
+++ precache-manifest.cbdd022bb842ed32e9664e62254feeab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "93b0cff99663c1a87a6d",
+    "url": "/ImageGalleryReact/static/css/main.ff815c8f.chunk.css"
+  },
+  {
+    "revision": "93b0cff99663c1a87a6d",
+    "url": "/ImageGalleryReact/static/js/main.93b0cff9.chunk.js"
+  },
+  {
+    "revision": "3a79397ac1a1b3a66e79",
+    "url": "/ImageGalleryReact/static/js/1.3a79397a.chunk.js"
+  },
+  {
+    "revision": "70e73d1c0a4841f5fa24",
+    "url": "/ImageGalleryReact/static/css/2.aeded344.chunk.css"
+  },
+  {
+    "revision": "70e73d1c0a4841f5fa24",
+    "url": "/ImageGalleryReact/static/js/2.70e73d1c.chunk.js"
+  },
+  {
+    "revision": "14cd50dd41cc8dcc119d",
+    "url": "/ImageGalleryReact/static/js/runtime~main.14cd50dd.js"
+  },
+  {
+    "revision": "982ec19a68eacf52cd27510dae986da5",
+    "url": "/ImageGalleryReact/index.html"
+  }
+];
